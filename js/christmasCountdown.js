23d2c5af77db48857e6833adb8428e341c92ad4f
Khariza <kharizzakaye@gmail.com>
2022-10-27 16:15:26 +0800

    Add files via upload

---
+++ js/christmasCountdown.js
@@ -0,0 +1,26 @@
+var christmasDayThisYear = new Date((new Date().getFullYear()), 11, 25);
+var countDownDate = new Date(christmasDayThisYear).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown-timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown-timer").innerHTML = "Merry Christmas!";
+  }
+}, 1000);
